c387d78c71548e082e5b10b12903c7559486a24c
Васильев Андрей <and7849w@yandex.ru>
2020-12-03 11:03:58 +0300

    add lang

--- src/api/ClientApi.js
+++ src/api/ClientApi.js
@@ -1,6 +1,6 @@
 /**
  * Groomer Service
- * API для будущей GroomCRM или назовите ее уже как-нибудь. На этой странице расписаны основные эндпоинты, по которым можно получить данные из базы данных (или положить их туда, если будет такая возможность). Также здесь можно будет протестировать эти самые эндпоинты, посмотреть ответы и всякое такое.  ### Changelog  **v1.2.2**: Добавлена сущность Салонов - географических расположений точек обслуживания клиентов, к которым привязываются мастера. Для получения списка мастеров салона добавлен фильтр salon_id  **v1.2.1**: Добавил эндпоинт для получения информации об авторизованном Клиенте/Мастере  **v1.2.0**: Обновлены пути, респонсы, эндпоинты для приложений вынесены в отдельный стек  **v1.1.4**: Обновил структуру WorkingDiapason  **v1.1.3**: Добавил описания возвращаемых кодов.  **v1.1.2**: Удалил упоминания Питомцев и Пушей из АПИ  **v1.1.1**: Добавил параметр \"платформа\" для заказа, заменил OneSignal на FCM + APNs  **v1.1.0**: Убрал пуши из API  **v1.0.4**: добавлены фильтры по датам, добавлено поле телефона для мастеров (для смс-оповещений), добавлено поле push_device_id для отправки пушей на телефон. 
+ * API для будущей GroomCRM или назовите ее уже как-нибудь. На этой странице расписаны основные эндпоинты, по которым можно получить данные из базы данных (или положить их туда, если будет такая возможность). Также здесь можно будет протестировать эти самые эндпоинты, посмотреть ответы и всякое такое.  ### Changelog  **v1.2.2**: Добавлена сущность Салонов - географических расположений точек обслуживания клиентов, к которым привязываются мастера. Для получения списка мастеров салона добавлен фильтр salon_id  **v1.2.1**: Добавил эндпоинт для получения информации об авторизованном Клиенте/Мастере  **v1.2.0**: Обновлены пути, респонсы, эндпоинты для приложений вынесены в отдельный стек  **v1.1.4**: Обновил структуру WorkingDiapason  **v1.1.3**: Добавил описания возвращаемых кодов.  **v1.1.2**: Удалил упоминания Питомцев и Пушей из АПИ  **v1.1.1**: Добавил параметр \"платформа\" для заказа, заменил OneSignal на FCM + APNs  **v1.1.0**: Убрал пуши из API  **v1.0.4**: добавлены фильтры по датам, добавлено поле телефона для мастеров (для смс-оповещений), добавлено поле push_device_id для отправки пушей на телефон.
  *
  * The version of the OpenAPI document: 1.2.1
  * Contact: kosolapus@gmail.com
@@ -24,7 +24,7 @@ import Salon from '../model/Salon';
 export default class ClientApi {
 
     /**
-    * Constructs a new ClientApi. 
+    * Constructs a new ClientApi.
     * @alias module:api/ClientApi
     * @class
     * @param {module:ApiClient} [apiClient] Optional API client implementation to use,
@@ -44,8 +44,8 @@ export default class ClientApi {
      */
 
     /**
-     * 
-     * 
+     *
+     *
      * @param {Number} clientID ID аккаунта
      * @param {module:api/ClientApi~clientClientIDDeleteCallback} callback The callback function, accepting three arguments: error, data, response
      */
@@ -129,8 +129,8 @@ export default class ClientApi {
      */
 
     /**
-     * 
-     * 
+     *
+     *
      * @param {Number} clientID ID аккаунта
      * @param {Object} opts Optional parameters
      * @param {Number} opts.id Уникальный идентификатор Клиента
@@ -138,11 +138,11 @@ export default class ClientApi {
      * @param {Boolean} opts.type Тип клиента. 0 - частный мастер, 1 - компания
      * @param {String} opts.name Название Клиента
      * @param {String} opts.email Электронная почта
-     * @param {Array.<module:model/Salon>} opts.salons 
+     * @param {Array.<module:model/Salon>} opts.salons
      * @param {File} opts.image Логотип клиента
      * @param {File} opts.imageUpload Логотип клиента
      * @param {File} opts.appImageUpload Логотип приложения
-     * @param {Object} opts.settings 
+     * @param {Object} opts.settings
      * @param {module:api/ClientApi~clientClientIDPatchCallback} callback The callback function, accepting three arguments: error, data, response
      */
     clientClientIDPatch(clientID, opts, callback) {
@@ -166,6 +166,7 @@ export default class ClientApi {
         'type': opts['type'],
         'name': opts['name'],
         'email': opts['email'],
+        'lang': opts['lang'],
         'salons': this.apiClient.buildCollectionParam(opts['salons'], 'csv'),
         'image': opts['image'],
         'image_upload': opts['imageUpload'],
@@ -194,8 +195,8 @@ export default class ClientApi {
      */
 
     /**
-     * Возвращает список клиентов массивом 
-     * Возвращает массив из Клиентов при их существовании или пустой массив, если клиентов нет. Требует токен с уровнем прав как минимум администратора сервиса 
+     * Возвращает список клиентов массивом
+     * Возвращает массив из Клиентов при их существовании или пустой массив, если клиентов нет. Требует токен с уровнем прав как минимум администратора сервиса
      * @param {Object} opts Optional parameters
      * @param {Number} opts.limit Как много элементов должно возвращаться за один запрос (default to 25)
      * @param {Number} opts.offset Смещение от первого (default to 0)
@@ -237,19 +238,19 @@ export default class ClientApi {
      */
 
     /**
-     * Создает нового клиента 
-     * Создает нового Клиента. Для работы с этим эндпоинтом требуется доступ как минимум администратора сервиса 
+     * Создает нового клиента
+     * Создает нового Клиента. Для работы с этим эндпоинтом требуется доступ как минимум администратора сервиса
      * @param {Object} opts Optional parameters
      * @param {Number} opts.id Уникальный идентификатор Клиента
      * @param {String} opts.password Пароль клиента
      * @param {Boolean} opts.type Тип клиента. 0 - частный мастер, 1 - компания
      * @param {String} opts.name Название Клиента
      * @param {String} opts.email Электронная почта
-     * @param {Array.<module:model/Salon>} opts.salons 
+     * @param {Array.<module:model/Salon>} opts.salons
      * @param {File} opts.image Логотип клиента
      * @param {File} opts.imageUpload Логотип клиента
      * @param {File} opts.appImageUpload Логотип приложения
-     * @param {Object} opts.settings 
+     * @param {Object} opts.settings
      * @param {module:api/ClientApi~clientPostCallback} callback The callback function, accepting three arguments: error, data, response
      */
     clientPost(opts, callback) {
